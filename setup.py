4d07e1ef00b0f68dc2c908d73ff45a71076a946f
Muneebheree <muneebabdullah992@gmail.com>
2024-11-19 14:18:08 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function will return a list of requirements
+    """
+    HYPHEN_E_DOT = '-e .'
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # Remove trailing newlines and spaces
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='Muneeb',
+    author_email='muneebabdulah992@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
